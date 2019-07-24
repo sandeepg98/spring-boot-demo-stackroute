4afd2f6d6b7f22971d5b67af52ec1bb8ac0e9842
sangrov1 <sangrov1@in.ibm.com>
2019-07-25 04:41:46 +0530

    first commit

---
+++ src/main/java/com/stackroute/userservice/domain/User.java
@@ -0,0 +1,23 @@
+package com.stackroute.userservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    int id;
+    String firstName;
+    String lastName;
+    int age;
+}
---
+++ src/main/java/com/stackroute/userservice/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.stackroute.userservice.service;
+
+import com.stackroute.userservice.domain.User;
+import com.stackroute.userservice.exceptions.UserAlreadyExistsException;
+import com.stackroute.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User saveUser(User user) throws UserAlreadyExistsException {
+        if(userRepository.existsById(user.getId()))
+            throw new UserAlreadyExistsException("User Already Exists");
+
+        User savedUser = userRepository.save(user);
+
+        if (savedUser == null)
+            throw new UserAlreadyExistsException("User is null");
+
+        return savedUser;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
